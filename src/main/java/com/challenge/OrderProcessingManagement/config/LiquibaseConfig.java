9415e35b98c18218524dca2498a6e8886617c73a
Lucas Duarte <lucasduarteinacio@hotmail.com>
2025-12-03 19:49:07 -0300

    adjusts

---
+++ src/main/java/com/challenge/OrderProcessingManagement/config/LiquibaseConfig.java
@@ -0,0 +1,28 @@
+package com.challenge.OrderProcessingManagement.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    @DependsOn("dataSource")
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.yaml");
+        liquibase.setShouldRun(true);
+        liquibase.setDropFirst(false);
+        return liquibase;
+    }
+}
+
