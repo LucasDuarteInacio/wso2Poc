25f787438e4b087b379a21310cd0f9c5a95ca411
Lucas Duarte <lucasduarteinacio@hotmail.com>
2025-12-03 23:33:09 -0300

    improvements

--- src/main/java/com/challenge/OrderProcessingManagement/config/OpenApiConfig.java
+++ src/main/java/com/challenge/OrderProcessingManagement/config/OpenApiConfig.java
@@ -4,8 +4,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.OpenAPIV3Parser;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import lombok.SneakyThrows;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -18,12 +16,12 @@ public class OpenApiConfig {
     @Bean
     @SneakyThrows
     public OpenAPI customOpenAPI() {
-            ClassPathResource resource = new ClassPathResource("openapi.yaml");
-            String openApiContent = new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+        ClassPathResource resource = new ClassPathResource("openapi.yaml");
+        String openApiContent = new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
 
-            OpenAPIV3Parser parser = new OpenAPIV3Parser();
-            SwaggerParseResult parseResult = parser.readContents(openApiContent, null, null);
-                return parseResult.getOpenAPI();
+        OpenAPIV3Parser parser = new OpenAPIV3Parser();
+        SwaggerParseResult parseResult = parser.readContents(openApiContent, null, null);
+        return parseResult.getOpenAPI();
     }
 }
 
